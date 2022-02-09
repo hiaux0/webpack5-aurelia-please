3b2a474ac2a9795a39ece0f6f454590babfad2d9
hiaux0 <hieudn5293@yahoo.de>
2022-02-09 12:02:36 +0100

    feat(lit): import

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
+import * as LitJsSdk from 'lit-js-sdk'
+/* prettier-ignore */ console.log('TCL ~ file: app.ts ~ line 2 ~ LitJsSdk', LitJsSdk)
+
+declare global {
+  interface Window {
+    litNodeClient: unknown;
+  }
+}
 
 export class App {
-  message = 'Hello'
+  message = "Hello";
 
+  bind() {
+    // document.addEventListener(
+    //   "lit-ready",
+    //   function (e) {
+    //     console.log("LIT network is ready");
+    //     // setNetworkLoading(false); // replace this line with your own code that tells your app the network is ready
+    //   },
+    //   false
+    // );
+  }
+
+  async attached() {
+    // const client = new LitJsSdk.LitNodeClient();
+    // await client.connect();
+    // window.litNodeClient = client;
+  }
 }
